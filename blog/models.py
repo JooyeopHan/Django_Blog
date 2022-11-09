80e1a5f9578fd34e8642998f00773efef1208ada
JooyeopHan <jooeyeop96@gmail.com>
2022-11-09 12:11:18 +0900

    author 필드 추가하기

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 
+# 작성자 표시
+from django.contrib.auth.models import User
+
 # 첨부 파일명과 확장자 아이콘 나타낼떄 사용(get_file_name() 함수)
 import os
 
@@ -16,6 +19,10 @@ class Post(models.Model):
     head_image = models.ImageField(upload_to="blog/images/%Y/%m/%d/", blank=True)
     file_upload = models.FileField(upload_to = 'blog/files/%Y/%m/%d/', blank=True)
 
+    # on_delete = models.CASECADE ==> 이 포스트의 작성자가 데이터 베이스에서 삭제되었을 떄 이 포스트도 같이 삭제
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -23,7 +30,7 @@ class Post(models.Model):
 
 # admin의 포스트 pk, title 노출
     def __str__(self):
-        return f'[{self.pk}]{self.title}'
+        return f'[{self.pk}]{self.title} :: {self.author}'
 
     def get_absolute_url(self):
         return f'/blog/{self.pk}/'
